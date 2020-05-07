045064ecbb155a6d5dc548bad3378f38ea32a164
Samanthavermeulen <s.vermeulen@caesar.nl>
2020-05-07 16:06:14 +0200

    added the project

---
+++ script.js
@@ -0,0 +1,36 @@
+class MaxBh {
+  constructor() {
+    this.value = [];
+  }
+
+  insert(element) {
+    let val = this.value;
+    val.push(element);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let value = this.value,
+      index = value.length - 1,
+      elem = value[index]; // type of loop??
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2),
+        parent = value[parentIndex];
+      if (elem <= parent) break;
+      value[parentIndex] = elem;
+      value[index] = parent;
+      index = parentIndex;
+    }
+  }
+}
+
+let maxBh = new MaxBh();
+
+maxBh.insert(41);
+maxBh.insert(39);
+maxBh.insert(33);
+maxBh.insert(18);
+maxBh.insert(27);
+maxBh.insert(12);
+maxBh.insert(55);
+console.log(maxBh);
